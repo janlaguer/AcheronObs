85f517b8693155debb072516c585a244b6be4c32
Jan Jozef Laguer <jan.laguer@obf.ateneo.edu>
2021-06-09 18:14:38 +0800

    remastered and optimized top hud

--- frontend/JsonDerulo.js
+++ frontend/JsonDerulo.js
@@ -6,7 +6,7 @@ function updatePlayers(json) {
     for (var x = 0; x <=2; x++) {
         for (var i = 0; i <= 4; i++) {
             // console.log(json.teams[0].players[i].id)
-            document.getElementById(`t${x}p${json.teams[0].players[i].id}`).innerText = json.teams[0].players[i].display_name.toUpperCase()
+            document.getElementById(`t${x}p${i}`).innerText = json.teams[0].players[i].display_name.toUpperCase()
         }
     }
 }
@@ -14,8 +14,8 @@ function updatePlayers(json) {
 function updateRound(json) {
     const num1 = parseInt(json.teams[0].game_score, 10)
     const num2 = parseInt(json.teams[1].game_score, 10)
-    document.getElementById('t0score').innerHTML = num1
-    document.getElementById('t1score').innerHTML = num2
+    document.getElementById('team0_score').innerHTML = num1
+    document.getElementById('team1_score').innerHTML = num2
     document.getElementById('roundCount').innerHTML = `ROUND ${num1 + num2 + 1}`
 }
 
